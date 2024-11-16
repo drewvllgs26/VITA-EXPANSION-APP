6a47e6ff4aa1ee4602ca23a2da30c3a9fa860a2e
Andrew James Villegas <andrewvillegas610@gmail.com>
2024-11-16 14:00:22 +0800

    nov 16 latest

--- Assets/TimerScript.cs
+++ Assets/TimerScript.cs
@@ -6,59 +6,65 @@ using TMPro;
 
 public class TimerScript : MonoBehaviour
 {
-    public float timeRemaining = 180;  // Set to 3 minutes (180 seconds)
+    public float timeRemaining = 180;
     public bool timeIsRunning = false;
     public AudioSource audioSource;
     public TMP_Text timeText;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Check if the timeText is not assigned
         if (timeText == null)
         {
             Debug.LogError("Error: timeText is not assigned!");
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (timeIsRunning)
         {
             if (timeRemaining > 0)
             {
-                timeRemaining -= Time.deltaTime;  // Subtract to countdown
+                timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
             }
             else
             {
-                // Timer reaches zero, stop it
                 timeIsRunning = false;
                 timeRemaining = 0;
-                DisplayTime(timeRemaining);  // Ensure the time is shown as 00:00 when it finishes
+                DisplayTime(timeRemaining); 
                 audioSource.Play();
+                timeText.text = string.Format("Time's Up!");
+                timeText.fontSize = 120;
             }
         }
     }
 
-    // This function starts the timer when the button is clicked
+
     public void StartTimer()
     {
-        timeIsRunning = true;
+        timeIsRunning = !timeIsRunning;
     }
 
-    // This function will display the time in the format "minutes : seconds"
     void DisplayTime(float timeToDisplay)
     {
-    // Make sure timeToDisplay is not negative
+
     timeToDisplay = Mathf.Max(timeToDisplay, 0);
 
-    // Get minutes and seconds
-    float minutes = Mathf.FloorToInt(timeToDisplay / 60);  // Get minutes
-    float seconds = Mathf.FloorToInt(timeToDisplay % 60);  // Get seconds
+    float minutes = Mathf.FloorToInt(timeToDisplay / 60);  
+    float seconds = Mathf.FloorToInt(timeToDisplay % 60);  
 
-    // Display time in "MM : SS" format
+
     timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
+
+    public void ResetTimer()
+    {
+        timeIsRunning = false;
+        audioSource.Stop();
+        timeRemaining = 180;
+        timeText.text = string.Format("3:00");
+        timeText.fontSize = 230;
+    }
+
 }
