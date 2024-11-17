04f3607454dd715e394156a4a55a78b211f96fb7
Andrew James Villegas <andrewvillegas610@gmail.com>
2024-11-17 12:49:34 +0800

    final w/inc assets

---
+++ Assets/RandomEvent.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class RandomEvent
+{
+    public string title;       // Event Name
+    public string quote;       // Event Quote
+    public string description; // Event Info
+}
